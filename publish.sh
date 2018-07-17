a6bbf1349a5bfd6d3724567fb87cfb8ee3423a6f
Eric Smith <erics@dakcs.com>
2018-07-16 20:37:16 -0600

    Fix publish script to start the correct service

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 sudo killall /usr/bin/dotnet
 sudo dotnet publish -o /var/Stocks --configuration Release
-sudo systemctl restart kestrel-conway.service
+sudo systemctl restart kestrel-stocks.service
