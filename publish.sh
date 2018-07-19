55f98abeb7a8ed63ff9f3e80c9ba18ac01ad3a69
Eric Smith <erics@dakcs.com>
2018-07-18 20:30:12 -0600

    have publish run npm install

--- publish.sh
+++ publish.sh
@@ -1,3 +1,8 @@
 sudo killall /usr/bin/dotnet
 sudo dotnet publish -o /var/Stocks --configuration Release
+PrevDir=$(pwd)
+cd /var/Stocks
+sudo npm install
 sudo systemctl restart kestrel-stocks.service
+cd $PrevDir
+unset PrevDir
