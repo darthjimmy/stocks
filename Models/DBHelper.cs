e5f8567f0d76f5d5138a714e695739612190abdd
StevenBate <39107347+StevenBate@users.noreply.github.com>
2018-07-30 20:07:56 -0600

    purchase stock

--- Models/DBHelper.cs
+++ Models/DBHelper.cs
@@ -102,6 +102,99 @@ namespace stocks
             }
         }
         
+        public static bool PurchaseStock(string username, string ticker, int shares)
+        {
+            decimal balance = GetBalance(username);
+            decimal cost = StockPrice(ticker);
+            int newShares = 0;
+            int stockID = -1;
+            int userInvestmentsID = -1;
+            cost = cost * shares;
+            if (balance < cost)
+            {
+                return false;
+            }
+            decimal differance = balance - cost;
+            using (var conn = new MySqlConnection(connstring.ToString()))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = "UPDATE user SET balance = @difference WHERE username = @username";
+
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@difference", difference);
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "SELECT stockID FROM stocks WHERE ticker = @ticker";
+                    cmd.Parameters.AddWithValue("@ticker", ticker);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            stockID = reader.GetInt32("stockID");
+                        }
+                    }
+
+                    cmd.CommandText = "SELECT userInvestmentsID, shares FROM userInvestments WHERE stockID = @stockID";
+                    cmd.Parameters.AddWithValue("@stockID", stockID);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            userInvestmentsID = reader.GetInt32("userInvestmentsID");
+                            newShares = reader.GetInt32("shares");
+                        }
+                    }
+
+                    shares = shares + newShares;
+                    if (userInvestmentsID == -1)
+                    {
+                        cmd.CommandText = "INSERT INTO userInvestmentsID (userID, stockID, shares) VALUES (@userID, @stockID, @shares)";
+                    }
+                    else
+                    {
+                        cmd.CommandText = "UPDATE userInvestmentsID SET shares = @shares WHERE userID = @userID AND stockID = @stockID";
+                    }
+                    cmd.Parameters.AddWithValue("@shares", shares);
+
+                    if (cmd.ExecuteNonQuery() > 0) return true;
+                }
+            }
+            return false;
+        }
+
+        public static decimal StockPrice(string ticker)
+        {
+            decimal amount = 0;
+            using (var conn = new MySqlConnection(connstring.ToString()))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = "SELECT costPerShare FROM stocks WHERE ticker = @ticker";
+
+                    cmd.Parameters.AddWithValue("@ticker", ticker);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            amount = reader.GetDecimal("costPerShare");
+                        }
+                    }
+
+                    return amount;
+
+                }
+            }
+        }
+        
         public static bool AddBalance(decimal amount, string username)
         {
             using (var conn = new MySqlConnection(connstring.ToString()))
